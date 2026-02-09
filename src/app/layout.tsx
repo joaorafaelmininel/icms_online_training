ca256ed387577739b18c2881cf6f1d82400cd229
mininelj <joaomininel@gmail.com>
2026-02-09 13:04:01 -0300

    adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const roboto = Roboto({
 });
 
 export const metadata: Metadata = {
-  title: "INSARAG | Online Training,
+  title: "INSARAG | Online Training",
   description: "INSARAG Coordination and Management System 3.0 Online Training Platform",
 };
 
